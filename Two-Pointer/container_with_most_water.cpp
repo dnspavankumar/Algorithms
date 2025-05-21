9ea522c9452862e9aa5f56081540b64402f2e31b
dnspavankumar <dnspavankumar722@gmail.com>
2025-05-21 18:15:38 +0530

    Update container_with_most_water.cpp

--- Two-Pointer/container_with_most_water.cpp
+++ Two-Pointer/container_with_most_water.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-    int maxArea(vector<int>& heights) {
-        int ans=INT_MIN;
+       int maxArea(vector<int>& heights) {
+         int ans=INT_MIN;
         int i=0;
         int j=heights.size()-1;
         while(i<j){
-            ans=max(ans,min(heights[i],heights[j])*(i-j+1));
+            ans=max(ans,min(heights[i],heights[j])*(j-i));
             if(heights[i]<heights[j])i++;
             else j--;
         }
